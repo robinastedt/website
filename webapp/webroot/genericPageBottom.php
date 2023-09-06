bf33e37bc68befdea908805ef94cae9aed828024
Robin Åstedt <robin.astedt@gmail.com>
2023-09-06 23:28:37 +0200

    Update footer

--- webapp/webroot/genericPageBottom.php
+++ webapp/webroot/genericPageBottom.php
@@ -3,7 +3,7 @@
 <!--GENERIC BOTTOM START-->
 	</div>
 	<div id="bottomFillerBox"></div>
-	<div class="contentBox" id="bottomContentBox">CopyRight &copy; 2009 - 2020 <a href="mailto:robin.astedt@gmail.com">Robin &Aring;stedt</a></div>
+	<div class="contentBox" id="bottomContentBox">CopyRight &copy; 2009 - 2023 <a href="mailto:robin.astedt@gmail.com">Robin &Aring;stedt</a></div>
 	<?php echoReadabilitySwitchLink() ?>
 	</body>
 </html>
