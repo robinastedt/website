dc573198aa3435d0d49c4896f26ba7d21882b25e
Robin Åstedt <robin.astedt@gmail.com>
2024-03-03 14:14:35 +0100

    Move source code message to genericPageTop.php

--- webapp/webroot/genericPageTop.php
+++ webapp/webroot/genericPageTop.php
@@ -1,5 +1,14 @@
 <!--GENERIC TOP START-->
 
+<?php
+	echo "\n\n\n<!-- 
+	Excuse the mess if you're looking at this source code.
+	These pages are generated with PHP and may not look very pretty,
+	nor well-formatted, in their HTML format. PHP, HTML, and CSS are not
+	exactly my cup of tea; they were simply a necessary evil, a means to an end.
+	-->\n\n\n";
+?>
+
 <?php include_once("utilities.php"); ?>
 
 <html><head>
