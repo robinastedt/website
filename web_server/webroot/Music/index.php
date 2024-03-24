5a745dc550b51bd9f1fdb49f6d2682f453e7967c
Robin Åstedt <robin.astedt@gmail.com>
2024-03-24 19:08:29 +0100

    Add music, The tyranny of the mind

--- web_server/webroot/Music/index.php
+++ web_server/webroot/Music/index.php
@@ -7,7 +7,8 @@
 	$params = array(
 		1,
 		'Music',
-		12,
+		13,
+		'The tyranny of the mind',
 		'Ardor',
 		'Requiem for a childhood lost',
 		'Completely plucked up',
@@ -23,6 +24,11 @@
 	);
 	call_user_func_array('echoTitle', $params);
 ?>
+
+<?php echoNewContentBox(); ?>
+
+<h3 id="Thetyrannyofthemind">The tyranny of the mind</h3>
+<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=1307090357/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://robinastedt.bandcamp.com/track/tyranny-of-the-mind">Tyranny of the mind by Robin Åstedt</a></iframe>
 
 <?php echoNewContentBox(); ?>
 
@@ -42,47 +48,47 @@
 <?php echoNewContentBox(); ?>
 
 <h3 id="TravestyOfEphemerality">Travesty Of Ephemerality</h3>
-<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3313404005/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/the-travesty-of-ephemerality">The Travesty Of Ephemerality by Robin Åstedt</a></iframe>
+<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3313404005/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/the-travesty-of-ephemerality">The Travesty Of Ephemerality by Robin Åstedt</a></iframe>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="Journey">Journey</h3>
-<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3331464971/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/journey">Journey by Robin Åstedt</a></iframe>
+<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3331464971/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/journey">Journey by Robin Åstedt</a></iframe>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="EntertheUnknown">Enter the Unknown</h3>
-<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3331927362/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/enter-the-unknown">Enter the Unknown by Robin Åstedt</a></iframe>
+<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3331927362/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/enter-the-unknown">Enter the Unknown by Robin Åstedt</a></iframe>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="TakeTheWorld">Take The World</h3>
-<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=1487768127/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=false/" seamless><a href="http://robinastedt.bandcamp.com/track/take-the-world">Take The World by Robin &Aring;stedt</a></iframe></p>
+<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=1487768127/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=false/" seamless><a href="http://robinastedt.bandcamp.com/track/take-the-world">Take The World by Robin &Aring;stedt</a></iframe></p>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="AwayWithYou">Away With You</h3>
-<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3525315637/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/away-with-you">Away with You by Robin &Aring;stedt</a></iframe></p>
+<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=3525315637/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/away-with-you">Away with You by Robin &Aring;stedt</a></iframe></p>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="Dance">Dance</h3>
-<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=2194166892/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/dance">Dance by Robin &Aring;stedt</a></iframe></p>
+<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=2194166892/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/dance">Dance by Robin &Aring;stedt</a></iframe></p>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="Reckoning">Reckoning</h3>
-<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=812034379/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/reckoning">Reckoning by Robin &Aring;stedt</a></iframe></p>
+<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=812034379/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/reckoning">Reckoning by Robin &Aring;stedt</a></iframe></p>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="DoomE1M8ViolinSynthMix">Doom E1M8 Violin/Synth Mix</h3>
-<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=2846173504/size=large/bgcol=000000/linkcol=D3D3D3/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/doom-e1m8-violin-synth-mix">Doom E1M8 Violin/Synth Mix by Robin &Aring;stedt</a></iframe></p>
+<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=2846173504/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/doom-e1m8-violin-synth-mix">Doom E1M8 Violin/Synth Mix by Robin &Aring;stedt</a></iframe></p>
 
 <?php echoNewContentBox(); ?>
 
 <h3 id="Walk">Walk</h3>
-<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=2283873393/size=large/bgcol=000000/linkcol=6C0000/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/walk">Walk by Robin &Aring;stedt</a></iframe></p>
+<p><iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/track=2283873393/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://robinastedt.bandcamp.com/track/walk">Walk by Robin &Aring;stedt</a></iframe></p>
 
 <?php echoNewContentBox(); ?>
 
