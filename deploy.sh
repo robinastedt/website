c134b11ebbf1b8733c7e28a0666fd6826b7ab5a4
Robin Åstedt <robin.astedt@gmail.com>
2024-03-24 18:59:36 +0100

    Fix deploy script to copy files into directory

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -e
+
 WEBSERVER_HOSTNAME=webserver
 
-scp -r ./* ${WEBSERVER_HOSTNAME}:~/
+ssh ${WEBSERVER_HOSTNAME} mkdir -p ~/website
+scp -r ./* ${WEBSERVER_HOSTNAME}:~/website/
 ssh ${WEBSERVER_HOSTNAME} "cd website; ./install.sh"
